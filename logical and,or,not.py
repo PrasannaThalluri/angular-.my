17f79649a807ec946ed3cda14baabde5b4088831
unknown <AbstractProgrammer@example.com>
2024-08-06 07:41:45 +0530

    first commit

---
+++ logical and,or,not.py
@@ -0,0 +1,55 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+weight=20
+height=5.7
+print(weight/height * height)
+20.0
+a=5
+print(a)
+5
+a,b,c=1,2,3
+print(a,b,c)
+1 2 3
+a,b=4,5
+c= +=a
+SyntaxError: invalid syntax
+a += 2
+>>> print(a)
+6
+>>> a,b=4,3
+>>> c=a
+>>> a,b=4,3
+>>> c=a+b
+>>> a += 2
+>>> c += a
+>>> print(c)
+13
+>>> a=5
+>>> print(a==5)
+True
+>>> print(a<=5)
+True
+>>> print(a<5)
+False
+>>> print(a !=5)
+False
+>>> a,b=4,5
+>>> print(a<3 and b==3)
+False
+>>> print(a<3 0r b==3)
+SyntaxError: invalid decimal literal
+>>> print(a<3 or b==3)
+False
+>>> a,b=4,3
+>>> print(!a)
+SyntaxError: invalid syntax
+>>> print(! (a))
+SyntaxError: invalid syntax
+>>> print(not(a))
+False
+>>> False
+False
+>>> a,b=4,3
+>>> c=True
+>>> print(a<=4 and c)
+True
---
+++ relational operators.py
@@ -0,0 +1,35 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> weight=20
+>>> height=5.7
+>>> print(weight/height * height)
+20.0
+>>> a=5
+>>> print(a)
+5
+>>> a,b,c=1,2,3
+>>> print(a,b,c)
+1 2 3
+>>> a,b=4,5
+>>> c= +=a
+SyntaxError: invalid syntax
+>>> a += 2
+>>> print(a)
+6
+>>> a,b=4,3
+>>> c=a
+>>> a,b=4,3
+>>> c=a+b
+>>> a += 2
+>>> c += a
+>>> print(c)
+13
+>>> a=5
+>>> print(a==5)
+True
+>>> print(a<=5)
+True
+>>> print(a<5)
+False
+>>> print(a !=5)
+False
